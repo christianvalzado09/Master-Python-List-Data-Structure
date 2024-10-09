0c32e6f2bc904bdaf682ec483785948cbfb3bf17
christian valzado <christianvalzado2@gmail.com>
2024-10-09 00:09:29 -0700

    docs:Adding University list

---
+++ university_list.py
@@ -0,0 +1,29 @@
+#1
+university_name_list = ["University of the Philippines", "Ateneo de Manila University", "De La Salle University", "University of Santo Tomas", "Polytechnic University of the Philippines"]
+
+print(university_name_list)
+
+#2
+university_ID_list = [1, 2, 3, 4, 5]
+
+print(university_ID_list)
+
+#3
+university_location_list = ["Quezon City", "Quezon City", "Manila", "Manila", "Manila"]
+
+print(university_location_list)
+
+#4
+university_established_year_list = [1908, 1859, 1911, 1611, 1904]
+
+print(university_established_year_list)
+
+#5
+university_type_list = ["Public", "Private", "Private", "Private", "Public"]
+
+print(university_type_list)
+
+#6
+university_website_list = ["www.up.edu.ph", "www.ateneo.edu", "www.dlsu.edu.ph", "www.ust.edu.ph", "www.pup.edu.ph"]
+
+print(university_website_list)
