a5b6a4ff11048f1fdf3cb5c99047ac5e10ccd46c
christian valzado <christianvalzado2@gmail.com>
2024-10-09 00:04:20 -0700

    docs:Adding Book list

---
+++ book_list.py
@@ -0,0 +1,39 @@
+#1
+book_title_list = ["The Great Gatsby", "To Kill a Mockingbird", 1984, "The Catcher in the Rye", "A Brief History of Time"]
+
+print(book_title_list)
+
+#2
+book_author_list = ["F. Scott Fitzgerald", "Harper Lee", "George Orwell", "J.D. Salinger", "Stephen Hawking"]
+
+print(book_author_list)
+
+#3
+book_ID_list = [1, 2, 3, 4, 5]
+
+print(book_ID_list)
+
+#4
+book_genre_list = ["Fiction", "Fiction", "Dystopian", "Fiction", "Non-Fiction"]
+
+print(book_genre_list)
+
+#5
+book_published_year_list = [1925, 1960, 1949, 1951, 1988]
+
+print(book_published_year_list)
+
+#6
+book_ISBN_list = ["978-0743273565", "978-0060935467", "978-0451524935", "978-0316769488", "978-0553380163"]
+
+print(book_ISBN_list)
+
+#7
+book_stock_list = [20, 35, 40, 25, 10]
+
+print(book_stock_list)
+
+#8
+book_price_list = [15.99, 10.99, 9.99, 8.99, 18.99]
+
+print(book_price_list)
