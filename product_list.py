f8d27fb7b5c1ad0af8453b80b7a84d89ec8c9d65
christian valzado <christianvalzado2@gmail.com>
2024-10-08 23:57:12 -0700

    docs : Adding Product list

---
+++ product_list.py
@@ -0,0 +1,29 @@
+#1
+product_name_list = ["Laptop", "Desk Chair", "Smartwatch", "Notebook", "Running Shoes"]
+
+print(product_name_list)
+
+#2
+product_ID_list = [1, 2, 3, 4, 5]
+
+print(product_ID_list)
+
+#3
+product_category_list = ["Electronics", "Furniture", "Electronics", "Stationery", "Apparel"]
+
+print(product_category_list)
+
+#4
+product_price_list = [750, 100, 200, 5, 80]
+
+print(product_price_list)
+
+#5
+product_stock_list = [50, 200, 150, 500, 100]
+
+print(product_stock_list)
+
+#6
+product_supplier_email_list = ["supplier1@gmail.com", "supplier2@gmail.com", "supplier3@gmail.com", "supplier4@gmail.com", "supplier5@gmail.com"]
+
+print(product_supplier_email_list)
