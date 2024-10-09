5ae9f37b16351c4b9673345d369856689a3ef141
christian valzado <christianvalzado2@gmail.com>
2024-10-09 00:06:03 -0700

    docs:Adding Employee list

---
+++ employee_list.py
@@ -0,0 +1,24 @@
+#1
+employee_name_list = ["John Doe", "Jane Smith", "Mark Johnson", "Lisa Wong", "Paul McDonald"]
+
+print(employee_name_list)
+
+#2
+employee_ID_list = [1, 2, 3, 4, 5]
+
+print(employee_ID_list)
+
+#3
+employee_department_list = ["Sales", "Human Resources", "IT", "Marketing", "Finance"]
+
+print(employee_department_list)
+
+#4
+employee_age_list= [30, 25, 40, 28, 35]
+
+print(employee_age_list)
+
+#5
+employee_email_list = ["john.doe@company.com", "jane.smith@company.com", "mark.johnson@company.com", "lisa.wong@company.com", "paul.mcdonald@company.com"]
+
+print(employee_email_list)
