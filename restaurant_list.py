6f9c2d496bb8254073606f337abd160f53f089fd
christian valzado <christianvalzado2@gmail.com>
2024-10-09 00:01:32 -0700

    docs: Adding Restaurant list

---
+++ restaurant_list.py
@@ -0,0 +1,29 @@
+#1
+restaurant_name_list = ["Vikings Luxury Buffet", "Antonio's Restaurant", "Mesa Filipino Moderne", "Manam Comfort Filipino", "Ramen Nagi"]
+
+print(restaurant_name_list)
+
+#2
+restaurant_ID_list = [1, 2, 3, 4, 5]
+
+print(restaurant_ID_list)
+
+#3
+restaurant_location_list = ["Pasay City", "Tagaytay", "Makati City", "Quezon City", "Various Locations"]
+
+print(restaurant_location_list)
+
+#4
+restaurant_cuisine_type_list = ["Buffet", "Fine Dining", "Filipino", "Filipino", "Japanese"]
+
+print(restaurant_cuisine_type_list)
+
+#5
+restaurant_established_year_list = [2011, 2002, 2009, 2013, 2013]
+
+print(restaurant_established_year_list)
+
+#6
+restaurant_website_list = ["www.vikings.ph", "www.antoniosrestaurant.ph", "www.mesa.ph", "www.manam.ph", "www.ramennagi.com.ph"]
+
+print(restaurant_website_list)
